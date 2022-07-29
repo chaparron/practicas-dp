ea7040acb58e8fb887f42a5ec72feb2df1604fd2
Victor Gonzalez <vgonzalez@yopapp.me>
2022-07-29 12:23:34 +0200

    Fix SaleInformationResponse without SerialName annotations

--- sdk/src/main/kotlin/digitalpayments/sdk/model/SaleInformationResponse.kt
+++ sdk/src/main/kotlin/digitalpayments/sdk/model/SaleInformationResponse.kt
@@ -1,16 +1,11 @@
 package digitalpayments.sdk.model
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class SaleInformationResponse(
-    @SerialName(value = "BankId")
     val bankId: String,
-    @SerialName(value = "MerchantId")
     val merchantId: String,
-    @SerialName(value = "TerminalId")
     val terminalId: String,
-    @SerialName(value = "EncData")
     val encData: String
 )
